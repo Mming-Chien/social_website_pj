7bc2e20e9ca3798f3b36e74e0677399c17720790
Mming-Chien <113403394+Mming-Chien@users.noreply.github.com>
2022-11-03 15:56:41 +0700

    reset password pages

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -6,13 +6,28 @@ urlpatterns = [
 	#previous login url
 	#path('login/', views.user_login, name='login'),
 	#Login/Logout urls
-	path('login/', auth_views.LoginView.as_view(), name='login'),
-	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+#	path('login/', auth_views.LoginView.as_view(), name='login'),
+#	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
 	# Change password urls
-	path('password-change/', auth_views.PasswordChangeView.as_view(), name='passowrd_change'),
-	path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+#	path('password-change/', auth_views.PasswordChangeView.as_view(), name='passowrd_change'),
+#	path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), 
+#						name='password_change_done'),
+
+	# Reset password urls
+#	path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+#	path('passowrd-reset/done/', auth_views.PasswordResetDoneView.as_view(), 
+#			name='password_reset_done'),
+#	path('password-reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(),
+#			name='password_reset_confirm'),
+#	path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(),
+#			name='password_reset_complete'),
+	
+	# Urls for authentication
+	path('', include("django.contrib.auth.urls")),
 
 	path('', views.dashboard, name='dashboard'),
+
+
 
 ]
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -129,3 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL= 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+# Send email to console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
