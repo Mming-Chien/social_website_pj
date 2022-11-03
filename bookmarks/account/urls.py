ee017d1e75824e94564c5f3924c44ce077d145ca
Mming-Chien <113403394+Mming-Chien@users.noreply.github.com>
2022-11-03 14:46:18 +0700

    Change password pages

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
 	path('login/', auth_views.LoginView.as_view(), name='login'),
 	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
+	# Change password urls
+	path('password-change/', auth_views.PasswordChangeView.as_view(), name='passowrd_change'),
+	path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
 	path('', views.dashboard, name='dashboard'),
 
 ]
