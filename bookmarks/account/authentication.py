133e9435f1dc9281e91c1a280cc11eac07f8f2ee
Mming-Chien <113403394+Mming-Chien@users.noreply.github.com>
2022-11-10 16:24:41 +0700

    create profile for OAuth users

--- bookmarks/account/authentication.py
+++ bookmarks/account/authentication.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from account.models import Profile
 
 class EmailAuthBackend:
 	''' Authenticate using email address'''
@@ -18,3 +19,8 @@ class EmailAuthBackend:
 		except User.DoesNotExist:
 			return None
 			
+
+
+def create_profile(backend, user, *args, **kwargs):
+	''' Create user profile for social authentication '''
+	Profile.objects.get_or_create(user=user)
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -161,3 +161,18 @@ SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 # google client id
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '847321798785-33e5ferq8cvvn7a07e14mdr0jbc7ofk3.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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' # google client secret
+
+# default Social auth pipeline
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile', #Create user profile for social auth
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
+
